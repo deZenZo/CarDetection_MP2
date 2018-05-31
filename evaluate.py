0c106153edbcb903d01fd88cc98730610c9ec851
OtorSama <jemwelautor@gmail.com>
2018-05-31 11:51:49 +0800

    img with true and predicted labels

--- evaluate.py
+++ evaluate.py
@@ -1,27 +1,32 @@
-# -*- coding: utf-8 -*-
 
 import cv2
-import sys
+import time
+#import sys
 
 
+labels = open("labels.csv","r")
 cascade_src = 'cars.xml'
-img_src = sys.argv[1]
-
 car_cascade = cv2.CascadeClassifier(cascade_src)
 
 while True:
-    img = cv2.imread(img_src)
+    line = labels.readline()
+    if not line:
+        exit()
+
+    img_file,x1,y1,x2,y2 = line.replace("\n","").split(",")
+
+    img = cv2.imread("dataset/"+img_file)
+
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
-
+   
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)    
     
-    #x1,y1,x2,y2 = 35,66, 133, 128
-    #cv2.rectangle(img, (x1,y1),(x2,y2),(0,255,0),2)
+    cv2.rectangle(img, (int(x1),int(y1)),(int(x2),int(y2)),(0,255,0),2)
     cv2.imshow('car', img)
-    
-    if cv2.waitKey(33) == 27:
-        break
 
-cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
